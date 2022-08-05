44e97585bd7c5e6973de9f9e2cc31e66ecec4712
AraNeaLux <aranealux@gmail.com>
2022-08-05 09:00:24 -0400

    working include navbar files

---
+++ navbar.js
@@ -0,0 +1,18 @@
+document.write('\
+\
+    <!-- Load an icon library to show a hamburger menu (bars) on small screens -->\
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\
+\
+    <link href="./resources/css/navbar.css" type="text/css" rel="stylesheet">\
+    <div class="topnav" id="myTopnav">\
+      <a href="#home" class="active">Home</a>\
+      <a href="house/welcome/0.html">About</a>\
+      <a href="science/0.html">Science</a>\
+      <a href="perfume/0.html">Perfumery</a>\
+      <a href="tarot/0.html">Tarot</a>\
+      <a href="javascript:void(0);" class="icon" onclick="mobileScreenAdjust()">\
+        <i class="fa fa-bars"></i>\
+      </a>\
+    </div>\
+\
+');
