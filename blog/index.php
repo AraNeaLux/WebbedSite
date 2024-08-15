0f9bcf265dccef876b327e9a31d7f8c535048604
AraNeaLux <aranealux@gmail.com>
2024-08-15 17:26:22 -0400

    attempting php for blog page

---
+++ blog/index.php
@@ -0,0 +1,17 @@
+<?php require_once('includes/header.php') ?>
+    <title>LifeBlog | Home </title>
+</head>
+<body>
+    <!-- container - wraps whole page -->
+    <div class="container">
+        <!-- navbar -->
+        <?php include('includes/navbar.php') ?>
+        <!-- Page content -->
+        <div class="content">
+            <h2 class="content-title">Recent Articles</h2>
+            <hr>
+            <!-- more content still to come here ... -->
+        </div>
+        <!-- // Page content -->
+        <!-- footer -->
+        <?php include('includes/footer.php') ?>
