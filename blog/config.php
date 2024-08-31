f095f1090051ebb58caf47eea10b22525a62b39f
AraNeaLux <aranealux@gmail.com>
2024-08-30 21:47:50 -0400

    testing if password gets uploaded

--- blog/config.php
+++ blog/config.php
@@ -1,7 +1,7 @@
 <?php 
         session_start();
         // connect to database
-        $conn = mysqli_connect("localhost", "root", "", "blog");
+        $conn = mysqli_connect("localhost", "root", "testpassword", "blog");
 
         if (!$conn) {
                 die("Error connecting to database: " . mysqli_connect_error());
