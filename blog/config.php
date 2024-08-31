fc6d84f9288d08ffef5eac92ce109a83d5596c0d
AraNeaLux <aranealux@gmail.com>
2024-08-30 21:24:49 -0400

    removed config file

--- blog/config.php
+++ blog/config.php
@@ -8,5 +8,5 @@
         }
     // define global constants
         define ('ROOT_PATH', realpath(dirname(__FILE__)));
-        define('BASE_URL', 'http://127.0.0.1:8080/edsa-webbedsite/blog/');
+        define('BASE_URL', 'http://127.0.0.1:80/WebbedSite/blog/');
 ?>
